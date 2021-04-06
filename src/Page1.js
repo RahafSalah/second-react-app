d12b9c260dc03c55a9e4a90c0bec365ac4a811e9
RahafSalah <rahaf.elewi@gmail.com>
2021-04-06 10:55:25 +0300

    2nd commit

--- src/Page1.js
+++ src/Page1.js
@@ -1,34 +1,24 @@
 import React from "react";
 class Page1 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.component2 = React.createRef();
-  }
-
-  changeColor = () => {
-    this.component2.current.changeColor();
-  };
   render() {
     return (
       <div>
         <h2>Page1...</h2>
-        <Component1 onColorChange={this.changeColor}></Component1>
-        <Component2 ref={this.component2}></Component2>
+        <Component1 onColorChange={() => this.colorChange()}></Component1>
+        <Component2
+          setColorChange={(change) => (this.colorChange = change)}
+        ></Component2>
       </div>
     );
   }
 }
 
 class Component1 extends React.Component {
-  changeColor = () => {
-    this.props.onColorChange();
-  };
-
   render() {
     return (
       <div>
         <h3>Component1...</h3>
-        <button onClick={this.changeColor}>Change Color</button>
+        <button onClick={this.props.onColorChange}>Change Color</button>
       </div>
     );
   }
@@ -40,11 +30,13 @@ class Component2 extends React.Component {
     this.state = {
       color: "",
     };
+    this.changeColor = this.changeColor.bind(this);
+    this.props.setColorChange(this.changeColor);
   }
 
-  changeColor = () => {
+  changeColor() {
     this.setState({ color: "blue" });
-  };
+  }
 
   render() {
     return (
--- src/Page2.js
+++ src/Page2.js
@@ -1,40 +1,36 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 
-const page2Context = React.createContext();
 function Page2() {
-  const [color, changeColor] = useState();
-
+  let changeColor = null;
   return (
     <div>
       <h2>Page2...</h2>
-      <page2Context.Provider
-        value={{ data: color, onChangeColor: changeColor }}
-      >
-        <Component1></Component1>
-        <Component2></Component2>
-      </page2Context.Provider>
+      <Component1 onColorChange={() => changeColor()}></Component1>
+      <Component2
+        setColorChange={(change) => (changeColor = change)}
+      ></Component2>
     </div>
   );
 }
 
-function Component1() {
-  let context = useContext(page2Context);
-  function changeColor() {
-    context.onChangeColor("blue");
-  }
-
+function Component1(props) {
   return (
     <div>
       <h3>Component1...</h3>
-      <button onClick={changeColor}>Change Color</button>
+      <button onClick={props.onColorChange}>Change Color</button>
     </div>
   );
 }
 
-function Component2() {
-  let context = useContext(page2Context);
+function Component2(props) {
+  const [color, setColor] = useState();
+  props.setColorChange(changeColor);
+  function changeColor() {
+    setColor("blue");
+  }
+
   return (
-    <div style={{ backgroundColor: context.data }}>
+    <div style={{ backgroundColor: color }}>
       <h3>Component2...</h3>
     </div>
   );
