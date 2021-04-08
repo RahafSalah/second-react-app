8ac129625b58f5fafecace56cc3c859a8337da32
RahafSalah <rahaf.elewi@gmail.com>
2021-04-08 09:20:41 +0300

    3nd commit

--- src/Page1.js
+++ src/Page1.js
@@ -1,13 +1,21 @@
 import React from "react";
 class Page1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      component2Color: "",
+    };
+    this.changeColor = this.changeColor.bind(this);
+  }
+  changeColor() {
+    this.setState({ component2Color: "blue" });
+  }
   render() {
     return (
       <div>
         <h2>Page1...</h2>
-        <Component1 onColorChange={() => this.colorChange()}></Component1>
-        <Component2
-          setColorChange={(change) => (this.colorChange = change)}
-        ></Component2>
+        <Component1 onColorChange={this.changeColor}></Component1>
+        <Component2 color={this.state.component2Color}></Component2>
       </div>
     );
   }
@@ -25,22 +33,9 @@ class Component1 extends React.Component {
 }
 
 class Component2 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      color: "",
-    };
-    this.changeColor = this.changeColor.bind(this);
-    this.props.setColorChange(this.changeColor);
-  }
-
-  changeColor() {
-    this.setState({ color: "blue" });
-  }
-
   render() {
     return (
-      <div style={{ backgroundColor: this.state.color }}>
+      <div style={{ backgroundColor: this.props.color }}>
         <h3>Component2...</h3>
       </div>
     );
--- src/Page2.js
+++ src/Page2.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 
 function Page2() {
-  let changeColor = null;
+  const [component2Color, setColor] = useState();
+  function changeColor() {
+    setColor("blue");
+  }
   return (
     <div>
       <h2>Page2...</h2>
-      <Component1 onColorChange={() => changeColor()}></Component1>
-      <Component2
-        setColorChange={(change) => (changeColor = change)}
-      ></Component2>
+      <Component1 onColorChange={changeColor}></Component1>
+      <Component2 color={component2Color}></Component2>
     </div>
   );
 }
@@ -23,14 +24,8 @@ function Component1(props) {
 }
 
 function Component2(props) {
-  const [color, setColor] = useState();
-  props.setColorChange(changeColor);
-  function changeColor() {
-    setColor("blue");
-  }
-
   return (
-    <div style={{ backgroundColor: color }}>
+    <div style={{ backgroundColor: props.color }}>
       <h3>Component2...</h3>
     </div>
   );
